cac91daa767d52e033cdb649faa2552193b6a46c
Brian French <fr3nch13@gmail.com>
2023-02-25 02:58:46 -0500

    Update ColorHelper.php

--- src/View/Helper/ColorHelper.php
+++ src/View/Helper/ColorHelper.php
@@ -28,7 +28,7 @@ class ColorHelper extends Helper
         if ($hexCode[0] == '#') {
             $hexCode = substr($hexCode, 1);
         }
-        if (strlen($hexCode) === 3) { // @phpstan-ignore-line
+        if (strlen($hexCode) < 4 && strlen($hexCode) >2) { // WTF phpstan for php7.4???
             $hexCode = $hexCode[0] . $hexCode[0] . $hexCode[1] . $hexCode[1] . $hexCode[2] . $hexCode[2];
         }
         $r = hexdec($hexCode[0] . $hexCode[1]);
